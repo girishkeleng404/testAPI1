a32125ea547d028e9754d8d612f00b5594a1fb6e
Girish keleng <girishkeleng30@gmail.com>
2024-09-10 12:54:21 +0530

    Refactor userController.js and userRoute.js to include etUserById route

--- controller/userController.js
+++ controller/userController.js
@@ -3,6 +3,7 @@
 const { Sequelize } = require("sequelize");
 const user = require("../db/models/user");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 
 const getAllUser = catchAsync(async(req,res,next)=>{
@@ -21,4 +22,18 @@ const getAllUser = catchAsync(async(req,res,next)=>{
    })
 })
 
-module.exports ={getAllUser};
+
+const getUserById = catchAsync(async(req,res,next)=>{
+    const userId = req.params.id;
+    const result = await user.findByPk(userId, {where:{userType:{[Sequelize.Op.ne]: '0'}}});
+    if(!result){
+        return next(new AppError('No user found with this id', 400));
+
+    };
+    return res.status(200).json({
+        message: "success",
+        data:result,
+    })
+}) 
+
+module.exports ={getAllUser,getUserById};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { authentication, restrictTo } = require('../controller/authController');
-const { getAllUser } = require('../controller/userController');
+const { getAllUser, getUserById } = require('../controller/userController');
  
 const router = express.Router();
 
 
-router.route('/').get(authentication,restrictTo('0'),getAllUser);
+router.route('/')
+.get(authentication,restrictTo('0'),getAllUser);
+
+router.route('/:id')
+.get(authentication,restrictTo('0'),getUserById);
 
 module.exports = router;
